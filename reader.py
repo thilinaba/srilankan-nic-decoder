4964a7e2420f64f694d7d935c9c543737b2116c3
Thilina Balasooriya <thilinabalasooriya1@gmail.com>
2019-03-09 04:19:23 +0530

    Bug fix for leap year and add age output

--- reader.py
+++ reader.py
@@ -1,13 +1,13 @@
 import datetime
 import re
 
-nic = input("Enter the NIC number: ")
+nic = input("Enter first 5 digits of the NIC number: ")
 
 # Simple input validation
-pattern = re.compile("^[0-9]{9}[V,v,X,x]$")
+pattern = re.compile("^[0-9]{5}$")
 if not pattern.match(nic):
-    print("Invalid NIC number.")
-    print("Valid example: 940041234V, 940041234X")
+    print("Invalid input.")
+    print("Valid example: 94004")
     exit()
 
 
@@ -23,11 +23,12 @@ if int(year) > 30 :
 else:
     year = int("20" + year)
 
+
 # reduce one day if not a leap year
-if year%4 != 0:
+if year%4 != 0 and days > 59:
     days = days - 1
 
-
+# They simply adds 500 to the days if the NIC owner is female
 if days > 500:
     gender = "Female"
     days = days - 500
@@ -35,8 +36,11 @@ else:
     gender = "Male"
 
 
-birthday = (datetime.datetime(year, 1, 1) + datetime.timedelta(days - 1)).strftime("%A %d. %B %Y")
+# datetime.datetime(year, 1, 1) = time to the 1st of jan of the year
+# datetime.timedelta(days - 1) = time of the number of days. we -1 because 1st of jan is already included
+birthday = datetime.datetime(year, 1, 1) + datetime.timedelta(days - 1)
+age = int((datetime.datetime.now() - birthday) / datetime.timedelta(365))
 
-
-print ("Birthday: " + birthday)
-print ("Gender: " + gender)
+print("Birthday: " + birthday.strftime("%A %d. %B %Y"))
+print("Gender: " + gender)
+print("Age: " + str(age) + " years")
